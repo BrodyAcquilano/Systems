6982377d262633725da22a26a7ba1680c806f1c1
Brody Acquilano <brodya2621@gmail.com>
2025-08-06 08:47:28 -0400

    Update header to use faviocn.png and not emoji.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,11 @@ import "../styles/Header.css";
 function Header() {
   return (
     <header className="header">
-      <div className="logo left-logo">⚖️</div>
-      <div className="title">Systems Engineering for Social Policy: A Structural Guide to Inequality and Reform</div>
-      <div className="logo right-logo">⚖️</div>
+      <img src="/favicon.png" alt="Scales icon" className="logo left-logo" />
+      <div className="title">
+        Systems Engineering for Social Policy: A Structural Guide to Inequality and Reform
+      </div>
+      <img src="/favicon.png" alt="Scales icon" className="logo right-logo" />
     </header>
   );
 }
